8d5de398fab9c31f542e6dcc20ebb3de1393c372
Davide Serafini <124274535+dserafini@users.noreply.github.com>
2024-01-26 18:34:17 +0100

    Update and rename Main_IRIS_NOVA2.py to main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+from smlib import loader
+
+from chargebuffer import chargebuffer_fsm
+from chargeslider import chargeslider_fsm
+from topactuator import topactuator_fsm
+from central import central_fsm
+from bottomactuator import bottomactuator_fsm
+from dischargeslider import dischargeslider_fsm
+from dischargebuffer import dischargebuffer_fsm
+
+
+def main():
+
+    # load the fsms
+    l = loader()
+
+    l.load(chargebuffer_fsm,"chargebuffer_fsm")
+    l.load(chargeslider_fsm,"chargeslider_fsm")
+    l.load(topactuator_fsm,"topactuator_fsm")
+    l.load(central_fsm,"central_fsm")
+    l.load(bottomactuator_fsm,"bottomactuator_fsm")
+    l.load(dischargeslider_fsm,"dischargeslider_fsm")
+    l.load(dischargebuffer_fsm,"dischargebuffer_fsm")
+
+    # start execution
+    l.start()
+
+
+
+if __name__ == '__main__':
+    main()
